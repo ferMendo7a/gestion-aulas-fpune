7c245e0069c3e87efff17ef30c9a4387161ac3bd
Fernando Mendoza <fermza9@gmail.com>
2020-01-26 17:50:47 -0300

    Commit inicial. Creación del proyecto.

---
+++ src/main/java/com/devmp/gestionaulas/GestionAulasApplication.java
@@ -0,0 +1,13 @@
+package com.devmp.gestionaulas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionAulasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionAulasApplication.class, args);
+	}
+
+}
