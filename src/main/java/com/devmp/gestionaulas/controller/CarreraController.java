4b01d10894fc1f1a028085e4d73eaebeb296288b
Fernando Mendoza <fermza9@gmail.com>
2020-03-16 21:10:46 -0300

    Agregado Controller de Carrera y Materia

---
+++ src/main/java/com/devmp/gestionaulas/controller/CarreraController.java
@@ -0,0 +1,27 @@
+package com.devmp.gestionaulas.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.devmp.gestionaulas.model.Carrera;
+import com.devmp.gestionaulas.service.CarreraService;
+
+@RestController
+@RequestMapping(path = "/carrera")
+public class CarreraController {
+
+	@Autowired
+	private CarreraService service;
+
+	@GetMapping("/todos")
+	public ResponseEntity<List<Carrera>> getAll() {
+		List<Carrera> retorno = service.getAll();
+		return ResponseEntity.ok().body(retorno);
+	}
+
+}
---
+++ src/main/java/com/devmp/gestionaulas/controller/MateriaController.java
@@ -0,0 +1,27 @@
+package com.devmp.gestionaulas.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.devmp.gestionaulas.model.Materia;
+import com.devmp.gestionaulas.service.MateriaService;
+
+@RestController
+@RequestMapping(path = "/materia")
+public class MateriaController {
+
+	@Autowired
+	private MateriaService service;
+
+	@GetMapping("/todos")
+	public ResponseEntity<List<Materia>> getAll() {
+		List<Materia> retorno = service.getAll();
+		return ResponseEntity.ok().body(retorno);
+	}
+
+}
