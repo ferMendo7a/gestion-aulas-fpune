2725aa0f860932136201f685a98b064a6d17ac2b
Fernando Mendoza <fermza9@gmail.com>
2020-03-16 21:12:00 -0300

    Modificación para conexión con Postgres

--- src/main/java/com/devmp/gestionaulas/model/Distribucion.java
+++ src/main/java/com/devmp/gestionaulas/model/Distribucion.java
@@ -5,9 +5,11 @@ import java.util.Date;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.OneToOne;
+import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
@@ -23,7 +25,8 @@ public class Distribucion {
 
 	@Id
 	@Column(name = "id")
-	@GeneratedValue
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "SEQ_DISTRIBUCION_GENERATOR")
+	@SequenceGenerator(name = "SEQ_DISTRIBUCION_GENERATOR", sequenceName = "seq_distribucion", allocationSize = 1)
 	private Integer id;
 
 	@OneToOne
